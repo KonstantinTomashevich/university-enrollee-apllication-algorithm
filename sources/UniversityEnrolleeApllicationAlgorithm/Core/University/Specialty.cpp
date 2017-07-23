70ab24d7c981535b83283813e6a652202b4aae97
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-07-23 20:07:19 +0300

    Add Specialty.

---
+++ sources/UniversityEnrolleeApllicationAlgorithm/Core/University/Specialty.cpp
@@ -0,0 +1,111 @@
+#include "Specialty.hpp"
+#include <UniversityEnrolleeApllicationAlgorithm/Core/Enrollee/Enrollee.hpp>
+
+namespace UniversityEnrolleeApllicationAlgorithm
+{
+Specialty::Specialty (unsigned id) :
+    ReferenceCounted (),
+    id_ (id),
+    requiredExams_ (),
+    marksInCertificatePriority_ (),
+
+    enrolleesInFreeForm_ (),
+    enrolleesInPaidForm_ (),
+
+    maxEnrolleesInFreeForm_ (0),
+    maxEnrolleesInPaidForm_ (0)
+{
+
+}
+
+Specialty::~Specialty ()
+{
+
+}
+
+unsigned Specialty::GetId () const
+{
+    return id_;
+}
+
+const std::vector <std::vector <unsigned> > &Specialty::GetRequiredExams() const
+{
+    return requiredExams_;
+}
+
+void Specialty::SetRequiredExams (const std::vector <std::vector <unsigned> > &requiredExams)
+{
+    requiredExams_ = requiredExams;
+}
+
+const std::vector <unsigned> &Specialty::GetMarksInCertificatePriority () const
+{
+    return marksInCertificatePriority_;
+}
+
+void Specialty::SetMarksInCertificatePriority (const std::vector <unsigned> &marksInCertificatePriority)
+{
+    marksInCertificatePriority_ = marksInCertificatePriority;
+}
+
+const std::vector <Enrollee *> &Specialty::GetEnrolleesInFreeForm () const
+{
+    return enrolleesInFreeForm_;
+}
+
+const std::vector <Enrollee *> &Specialty::GetEnrolleesInPaidForm () const
+{
+    return enrolleesInPaidForm_;
+}
+
+void Specialty::AddEnrollee (Enrollee *enrollee)
+{
+    // TODO: Implement.
+}
+
+std::vector <Enrollee *> Specialty::RemoveExcessEnrollees ()
+{
+    std::vector <Enrollee *> excessEnrollees;
+    GetExcessEnrollees (excessEnrollees, STUDY_FORM_FREE);
+    GetExcessEnrollees (excessEnrollees, STUDY_FORM_PAID);
+
+    enrolleesInFreeForm_.erase (enrolleesInFreeForm_.begin () + maxEnrolleesInFreeForm_, enrolleesInFreeForm_.end () - 1);
+    enrolleesInPaidForm_.erase (enrolleesInPaidForm_.begin () + maxEnrolleesInPaidForm_, enrolleesInPaidForm_.end () - 1);
+    return excessEnrollees;
+}
+
+unsigned Specialty::GetMaxEnrolleesInFreeForm () const
+{
+    return maxEnrolleesInFreeForm_;
+}
+
+void Specialty::SetMaxEnrolleesInFreeForm (unsigned maxEnrolleesInFreeForm)
+{
+    maxEnrolleesInFreeForm_ = maxEnrolleesInFreeForm;
+}
+
+unsigned Specialty::GetMaxEnrolleesInPaidForm () const
+{
+    return maxEnrolleesInPaidForm_;
+}
+
+void Specialty::SetMaxEnrolleesInPaidForm (unsigned maxEnrolleesInPaidForm)
+{
+    maxEnrolleesInPaidForm_ = maxEnrolleesInPaidForm;
+}
+
+void Specialty::GetExcessEnrollees (std::vector <Enrollee *> &output, StudyForm studyForm)
+{
+    std::vector <Enrollee *> &enrollees = (studyForm == STUDY_FORM_FREE) ? enrolleesInFreeForm_ : enrolleesInPaidForm_;
+    unsigned maxEnrollees = (studyForm == STUDY_FORM_FREE) ? maxEnrolleesInFreeForm_ : maxEnrolleesInPaidForm_;
+
+    if (enrollees.size () > maxEnrollees)
+    {
+        for (auto iterator = enrollees.cbegin () + maxEnrollees;
+             iterator != enrollees.cend (); iterator++)
+        {
+            output.push_back (*iterator);
+        }
+    }
+}
+}
---
+++ sources/UniversityEnrolleeApllicationAlgorithm/Core/University/Specialty.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <vector>
+#include <UniversityEnrolleeApllicationAlgorithm/Core/Enrollee/StudyForm.hpp>
+#include <UniversityEnrolleeApllicationAlgorithm/Utils/ReferenceCounted.hpp>
+
+namespace UniversityEnrolleeApllicationAlgorithm
+{
+class Enrollee;
+class Specialty : public ReferenceCounted
+{
+public:
+    explicit Specialty (unsigned id);
+    virtual ~Specialty ();
+    unsigned GetId () const;
+
+    const std::vector <std::vector <unsigned> > &GetRequiredExams () const;
+    void SetRequiredExams (const std::vector <std::vector <unsigned> > &requiredExams);
+
+    const std::vector <unsigned> &GetMarksInCertificatePriority () const;
+    void SetMarksInCertificatePriority (const std::vector <unsigned> &marksInCertificatePriority);
+
+    const std::vector <Enrollee *> &GetEnrolleesInFreeForm () const;
+    const std::vector <Enrollee *> &GetEnrolleesInPaidForm () const;
+    void AddEnrollee (Enrollee *enrollee);
+    std::vector <Enrollee *> RemoveExcessEnrollees ();
+
+    unsigned GetMaxEnrolleesInFreeForm () const;
+    void SetMaxEnrolleesInFreeForm (unsigned maxEnrolleesInFreeForm);
+
+    unsigned GetMaxEnrolleesInPaidForm () const;
+    void SetMaxEnrolleesInPaidForm (unsigned maxEnrolleesInPaidForm);
+
+private:
+    void GetExcessEnrollees (std::vector <Enrollee *> &output, StudyForm studyForm);
+
+    unsigned id_;
+    /// List of required exams names hashes. Should be sorted by priority! Example.
+    /// 0: Math -- highest priority
+    /// 1: Physics
+    /// 2: [BelarusianLanguage; RussianLanguage] -- if two exams specified, best exam will be selected for enrollees comparision.
+    std::vector <std::vector <unsigned> > requiredExams_;
+    /// Priority for marks in certificate, used for comparing enrollees, which have same exams results.
+    std::vector <unsigned> marksInCertificatePriority_;
+
+    std::vector <Enrollee *> enrolleesInFreeForm_;
+    std::vector <Enrollee *> enrolleesInPaidForm_;
+
+    unsigned maxEnrolleesInFreeForm_;
+    unsigned maxEnrolleesInPaidForm_;
+};
+}
