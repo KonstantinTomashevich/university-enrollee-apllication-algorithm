d8c8494ee7ac7c2db1e3a494e0fea6c113f4edb1
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-09-01 20:29:40 +0300

    Add SharedContextGlobalKeys.

---
+++ sources/UEADB/Core/SharedContextGlobalKeys.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <UEAA/Utils/CStringToHash.hpp>
+
+namespace UEADB
+{
+namespace SharedContextGlobalKeys
+{
+const unsigned UNIVERSITY = UEAA::CStringToHash ("University");
+const unsigned DE_HASH_TABLE = UEAA::CStringToHash ("DeHashTable");
+}
+}
