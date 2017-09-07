df0087b6fea79ccef2610a10f8c8f11040e335c1
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-09-07 22:05:03 +0300

    SaveUniversity: fix typo.

--- sources/UEADB/CommandsExecutors/SaveUniversity.cpp
+++ sources/UEADB/CommandsExecutors/SaveUniversity.cpp
@@ -54,7 +54,7 @@ void PrintHelp ()
 {
     std::cout << "SaveUniversity brief:" << std::endl <<
         "    Saves university from given XML file." << std::endl <<
-        "    Saveed university will be stored to commands shared context." << std::endl << std::endl <<
+        "    Saved university will be stored to commands shared context." << std::endl << std::endl <<
         "SaveUniversity arguments:" << std::endl <<
         "    1. Path to XML file with <university> core tag." << std::endl;
 }
