183ad4f5359077df5fa31f18419a5038f43c241e
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-11-30 09:24:25 +0300

    Update EnrolleeChoice.

--- sources/UEAA/Core/Enrollee/EnrolleeChoice.cpp
+++ sources/UEAA/Core/Enrollee/EnrolleeChoice.cpp
@@ -6,16 +6,14 @@ namespace UEAA
 {
 EnrolleeChoice::EnrolleeChoice () :
     faculty_ (0),
-    specialty_ (0),
-    studyForm_ (STUDY_FORM_FREE)
+    specialty_ (0)
 {
 
 }
 
-EnrolleeChoice::EnrolleeChoice (unsigned faculty, unsigned specialty, StudyForm studyForm) :
+EnrolleeChoice::EnrolleeChoice (unsigned faculty, unsigned specialty) :
     faculty_ (faculty),
-    specialty_ (specialty),
-    studyForm_ (studyForm)
+    specialty_ (specialty)
 {
 
 }
@@ -45,35 +43,22 @@ void EnrolleeChoice::SetSpecialty (unsigned specialty)
     specialty_ = specialty;
 }
 
-StudyForm EnrolleeChoice::GetStudyForm () const
-{
-    return studyForm_;
-}
-
-void EnrolleeChoice::SetStudyForm (StudyForm studyForm)
-{
-    studyForm_ = studyForm;
-}
-
 void EnrolleeChoice::SaveToXML (tinyxml2::XMLDocument &document, tinyxml2::XMLElement *output, DeHashTable *deHashTable) const
 {
     output->SetAttribute ("faculty", deHashTable->DeHash (faculty_).c_str ());
     output->SetAttribute ("specialty", deHashTable->DeHash (specialty_).c_str ());
-    output->SetAttribute ("studyForm", studyForm_);
 }
 
 void EnrolleeChoice::LoadFromXML (tinyxml2::XMLElement *input, DeHashTable *deHashTable)
 {
     faculty_ = CStringToHash (input->Attribute ("faculty"), deHashTable);
     specialty_ = CStringToHash (input->Attribute ("specialty"), deHashTable);
-    studyForm_ = static_cast <StudyForm> (atoi (input->Attribute ("studyForm")));
 }
 
 bool EnrolleeChoice::operator == (const EnrolleeChoice &rhs) const
 {
     return faculty_ == rhs.faculty_ &&
-           specialty_ == rhs.specialty_ &&
-           studyForm_ == rhs.studyForm_;
+           specialty_ == rhs.specialty_;
 }
 
 bool EnrolleeChoice::operator != (const EnrolleeChoice &rhs) const
